c061f3b89abaf3c35db18c39162e1692e35acd13
Arthur <tuckqueiroz82@gmail.com>
2020-12-23 01:56:21 -0300

    Estilização das páginas inicial, de login, cadastro e deleção com bootstrap

--- client_management/settings.py
+++ client_management/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "clients",
     "home",
+    "bootstrapform",
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
